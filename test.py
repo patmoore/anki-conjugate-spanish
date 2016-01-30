89f7183ee80d2cbdb5630932e07eee6ac3961c89
Patrick Moore <patmoore@farreach.es>
2016-01-29 21:08:43 -0800

    test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import conjugate_spanish
+from conjugate_spanish.verb_dictionary import Verb_Dictionary_get
+
+def find_print_verb(verb_inf):
+    verb = Verb_Dictionary_get(verb_inf)
+    print repr(verb.inf_verb_string).decode("unicode-escape")
+    verb.print_irregular_tenses()
+    print repr(verb.overrides_applied()).decode("unicode-escape")
+    print '----------------------'
+    
+fivehundredone = [u'abatir', u'abrasar', u'abrazar', u'abrir', u'absolver',u'abtenerse']
+verb_inf = fivehundredone[len(fivehundredone)-1]
+find_print_verb(verb_inf)
+
+for verb_inf in fivehundredone:
+    find_print_verb(verb_inf)
+    
