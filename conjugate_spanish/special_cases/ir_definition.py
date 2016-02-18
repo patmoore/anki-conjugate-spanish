faf34f5df9cefbc7089699030587dea938d2e471
Patrick Moore <patmoore@farreach.es>
2016-02-18 12:45:18 -0800

    supply key for hardcoded irse/tener irregulars

--- conjugate_spanish/special_cases/ir_definition.py
+++ conjugate_spanish/special_cases/ir_definition.py
@@ -10,14 +10,21 @@ Past_Tense_IR_CO.override_tense_ending(Tenses.past_tense, six.u('i'), Persons.fi
 Past_Tense_IR_CO.override_tense_ending(Tenses.past_tense, six.u('e'), Persons.third_person_singular)
 Past_Tense_IR_CO.override_tense_ending(Tenses.past_tense, six.u('eron'), Persons.third_person_plural, documentation="missing i")
 
-_ir_conjugation_overrides = ConjugationOverride(parents=[Past_Tense_IR_CO, u'oy', u'yendo'])
+_ir_conjugation_overrides = ConjugationOverride(parents=[Past_Tense_IR_CO, u'oy', u'yendo'], key="ir_irregular")
 for person in Persons.all_except(Persons.first_person_singular):
     _ir_conjugation_overrides.override_tense_ending(Tenses.present_tense, Standard_Conjugation_Endings[Infinitive_Endings.ar_verb][Tenses.present_tense][person], person)     
 _ir_conjugation_overrides.override_tense_ending(Tenses.present_tense, six.u('ais'), Persons.second_person_plural, documentation="no accent on a")
 _ir_conjugation_overrides.override_tense_stem(Tenses.present_tense, six.u('v'))
 _ir_conjugation_overrides.override_tense(Tenses.incomplete_past_tense, [six.u('iba'), six.u('ibas'), six.u('iba'), six.u('ibamos'),six.u('ibais'), six.u('iban')])
 _ir_conjugation_overrides.override_tense_stem(Tenses.present_subjective_tense, six.u('vay'))
+_ir_conjugation_overrides.override_tense(Tenses.imperative_positive, u've', Persons.second_person_singular)
+_ir_conjugation_overrides.override_tense(Tenses.imperative_positive, u'vamos', Persons.first_person_plural)
 _ir_conjugation_overrides.override_tense_stem(Tenses.past_participle, u'')
 _ir_conjugation_overrides.override_tense_stem(Tenses.gerund, u'')
 Ir_Definition = Verb_Dictionary_add(u'ir', conjugation_overrides=_ir_conjugation_overrides, definition="to go")
-Irse_Definition = Verb_Dictionary_add(u'irse', conjugation_overrides=_ir_conjugation_overrides, definition="to go")
+
+# Note: inherit from parent is implicit
+_irse_conjugation_overrides = ConjugationOverride(key=u"irse_irregular")
+# Also note that the negative is "no nos vayamos" -- we don't have a way to handle that.
+_irse_conjugation_overrides.override_tense(Tenses.imperative_positive, u'vámo', Persons.first_person_plural, documentation=u"vámonos only for POSITIVE")
+Irse_Definition = Verb_Dictionary_add(u'irse', conjugation_overrides=_irse_conjugation_overrides, definition="to go")
--- conjugate_spanish/special_cases/tener_definition.py
+++ conjugate_spanish/special_cases/tener_definition.py
@@ -3,7 +3,7 @@ from conjugate_spanish.conjugation_override import ConjugationOverride
 from conjugate_spanish.constants import Tenses,Persons
 from conjugate_spanish.verb_dictionary import Verb_Dictionary_add
 
-_conjugation_overrides = ConjugationOverride(parents=[u"go",u"e:ie", u'e_and_o', u'e2d'], key="tener_irregular")
+_conjugation_overrides = ConjugationOverride(parents=[u"go",u"e:ie", u'e_and_o', u'e2d'], key="tener")
 _conjugation_overrides.override_tense_stem(Tenses.present_tense, u'ten', Persons.first_person_singular, documentation="no e:ie in first person")
 _conjugation_overrides.override_tense_stem(Tenses.past_tense, u'tuv')
 _conjugation_overrides.override_tense(Tenses.imperative_positive, u'ten', Persons.second_person_singular)
