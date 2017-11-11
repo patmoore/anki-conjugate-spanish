bc16f069df4fbfd0c3d08df9406ce14b1e20542e
Patrick Moore <patmoore@farreach.es>
2017-11-11 14:27:08 -0800

    move printing of verb out of verb.py

--- conjugate_spanish/verb.py
+++ conjugate_spanish/verb.py
@@ -191,58 +191,6 @@ class Verb(Phrase):
     @classmethod        
     def is_verb(cls, phrase_string):
         return PhraseGroup.is_verb(phrase_string)
-                
-    def print_all_tenses(self):
-        conjugations= self.conjugate_all_tenses()
-        self._print_conjugations(conjugations)
-    
-    def print_irregular_tenses(self):
-        conjugations = self.conjugate_irregular_tenses()
-        self._print_conjugations(conjugations)
-        
-    def _print_conjugations(self, conjugations):
-        if conjugations is not None:        
-            for tense in range(len(Tenses)):
-                if conjugations[tense] is None:
-                    continue
-                
-                print("  "+ Tenses[tense], end=": ")
-                if tense in Tenses.Person_Agnostic:
-                    print(conjugations[tense])
-                else:
-                    for person in range(len(Persons)):
-                        if conjugations[tense][person] is not None:
-                            if not self.is_reflexive:
-                                print( Persons[person]+" "+conjugations[tense][person], end="; ")
-                            elif tense not in Tenses.imperative:
-                                print( conjugations[tense][person], end="; ")
-                            else:
-                                print(conjugations[tense][person])
-                                 
-                    print()
-
-    def print_csv(self, full_info=True):
-        result = '"'+self.full_phrase+'"'
-        if full_info:
-            if len(self.appliedOverrides) > 0:
-                result+=',"'+repr(self.appliedOverrides)+'"'
-            if len(self.doNotApply) > 0:
-                result +=',"'+repr(self.doNotApply)+'"'
-            if self.base_verb_string is not None:
-                result += ',"'+self.base_verb_string+'"'
-        
-        for tense in Tenses.all:
-            if tense in Tenses.Person_Agnostic:
-                conjugation = self.conjugate(tense)
-                result += ',"'+conjugation+'"'
-            else:
-                for person in Persons.all:
-                    conjugation = self.conjugate(tense, person)
-                    if conjugation is None:
-                        result += ','
-                    else:
-                        result += ',"'+conjugation+'"'
-        return result
         
     def conjugate_irregular_tenses(self):        
         """
