30a13e4da53d6f7107f554afefd0d7dc7ca91886
Patrick Moore <patmoore@farreach.es>
2017-11-19 19:07:11 -0800

    remove estar

--- conjugate_spanish/special_cases/__init__.py
+++ conjugate_spanish/special_cases/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import ir_definition
 from . import ser_definition
-from . import estar_definition
-__all__ = ['Ir_Definition', 'Ser_Definition', 'Estar_Definition']
+__all__ = ['Ir_Definition', 'Ser_Definition']
