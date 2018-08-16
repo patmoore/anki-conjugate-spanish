73da35be2307829ccb904ca57896d8227f2b8b5d
Patrick Moore <patmoore@farreach.es>
2018-08-15 19:35:34 -0700

    wip

--- conjugate.py
+++ conjugate.py
@@ -1,62 +1,96 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# See github page to report issues or to contribute:
 import sys
 import re
 from conjugate_spanish.constants import re_compile
 from conjugate_spanish import Espanol_Dictionary
-from conjugate_spanish.constants import Tense, Tenses, Persons, Person,\
-    IrregularNature
+from conjugate_spanish.constants import Tense, Tenses, Persons,\
+    IrregularNature, IrregularNatures
 from conjugate_spanish.phrase_printer import ScreenPrinter, CsvPrinter
-Espanol_Dictionary.load()
+import argparse
+
+class EnumAction(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        super(EnumAction, self).__init__(option_strings, dest, **kwargs)
+    def __call__(self, parser, namespace, values, option_string=None):
+        prev = getattr(namespace, self.dest) if hasattr(namespace, self.dest) else []
+        prev +=  self._enum.index(int(values))
+        setattr(namespace, self.dest, prev)
+        
+def tenseAction(**kwargs):
+    action = EnumAction(**kwargs)
+    action._enum = Tenses
+    return action
+
+def personAction(**kwargs):
+    action = EnumAction(**kwargs)
+    action._enum = Persons
+    return action
+ 
+def irregularAction(**kwargs):
+    action = EnumAction(**kwargs)
+    action._enum = IrregularNatures
+    return action       
+
+#Haya (hacer), Vaya (Ir), Sepa (Saber), Sea (Ser), Dé (Dar), éste (estar) 
+
+parser = argparse.ArgumentParser(description='Conjugate spanish verbs')
+parser.add_argument('phrases', metavar='phrase', type=str, nargs='*',
+                    default=None,
+                    help='a verb to conjugate')
+parser.add_argument('--printer', dest='printer_clazz', action='store_const',
+                    const=CsvPrinter, default=ScreenPrinter,
+                    help='print in csv format')
+parser.add_argument('--tenses', dest='tenses', 
+                    action=tenseAction,
+                    nargs='*',
+                    default=Tenses.all,
+                    help='select tenses')
+parser.add_argument('--persons', dest='persons',
+                    action=personAction,
+                    default=Persons.all,
+                    help='select persons')
+parser.add_argument('--irregular', dest='irregular_nature', 
+                    action=irregularAction,
+                    default=IrregularNatures.regular,
+                    help='select minimum irregularity for expansion.')
+args = parser.parse_args()
+
+
 options={}
-irregular_nature=IrregularNature.radical_stem_change
-if len(sys.argv) < 2:
-    sorted_keys = list(Espanol_Dictionary.verbDictionary.keys())
+verb=None
+
+spanishize_array = [
+        [ re_compile("'a"), 'á'],
+        [ re_compile("'e"), 'é'],
+        [ re_compile("'i"), 'í'],
+        [ re_compile("'o"), 'ó'],
+        [ re_compile("'u"), 'ú'],
+        [ re_compile("~u"), 'ü'],
+        [ re_compile("~n"), 'ñ']
+    ]
+
+def spanishize(phrase):
+    result = phrase
+    for operation, replacement in spanishize_array:
+        result = re.sub(operation, replacement, result)
+    return result
+
+def print_all():
+    if args.phrases is None:
+        sorted_keys = list(Espanol_Dictionary.verbDictionary.keys())
+    else:
+        sorted_keys = list(args.phrases)
     sorted_keys.sort()
     for key in sorted_keys:
-        phrase = Espanol_Dictionary.verbDictionary.get(key)
+        phrase = Espanol_Dictionary.get(key)
         if not phrase.is_phrase:
-            printer = CsvPrinter(phrase, irregular_nature=irregular_nature, options=options)
-            printer.print(tenses=Tenses.all)
-    exit(0)
+            printer = args.printer_clazz(phrase, irregular_nature=args.irregular_nature, options=options)
+            printer.print(tenses=args.tenses, persons=args.persons)
     
-arg_str = sys.argv[1]
-accent_a = re_compile("'a")
-arg_str = re.sub(accent_a, 'á', arg_str)
-accent_e = re_compile("'e")
-arg_str = re.sub(accent_e, 'é', arg_str)
-accent_i = re_compile("'i")
-arg_str = re.sub(accent_i, 'í', arg_str)
-accent_o = re_compile("'o")
-arg_str = re.sub(accent_o, 'ó', arg_str)
-accent_u = re_compile("'u")
-arg_str = re.sub(accent_u, 'ú', arg_str)
-umlaut_u = re_compile("~u")
-arg_str = re.sub(umlaut_u, 'ü', arg_str)
-ny = re_compile("~n")
-phrase_str = re.sub(ny, 'ñ', arg_str)
-phrase = Espanol_Dictionary.get(phrase_str)
-if phrase is None:
-    print(phrase_str+ " : not in dictionary")
-    exit(1)
-
-if len(sys.argv) < 3:
-    printer = ScreenPrinter(phrase, irregular_nature=irregular_nature, options=options)
-    printer.print()
-    
-    exit(0)    
-
-tenseIndex=int(sys.argv[2])
-tense = Tenses[tenseIndex]
-if len(sys.argv) < 4:
-    printer = ScreenPrinter(phrase, irregular_nature=IrregularNature.regular, options=options)
-    printer.print(tenses=[tense])
-    exit(0)
-
-personIndex = int(sys.argv[3])
-person = Persons[personIndex]
-print(tense.human_readable + ", " + person.human_readable)
-print(phrase.conjugate(tenseIndex, personIndex).full_conjugation)
-print(phrase.conjugation_tracking.get_conjugation_notes(tenseIndex, personIndex))
+Espanol_Dictionary.load()
+print_all()
+# print(tense.human_readable + ", " + person.human_readable)
+# print(phrase.conjugate(tenseIndex, personIndex).full_conjugation)
+# print(phrase.conjugation_tracking.get_conjugation_notes(tenseIndex, personIndex))
 
