f2f1b409554349d9c50c8095a9232dfb3576709c
Pat Moore <patrick@distribute.com>
2018-10-29 12:04:00 -0700

    sayprinter

--- conjugate_spanish/phrase_printers/say_printer.py
+++ conjugate_spanish/phrase_printers/say_printer.py
@@ -6,6 +6,10 @@ from .phrase_printer import PhrasePrinter
 
 
 class SayPrinter(PhrasePrinter):
+
+    def _print_tense_header(self, tense):
+        print("  {} ({}):".format(tense.human_readable, str(tense._value_)))
+
     def _print_conjugation_notes(self, conjugation_notes):
         print(conjugation_notes.full_conjugation)
-        os.system("say -v Monica {}".format(conjugation_notes.full_conjugation))
+        os.system("say -i -v Monica {}".format(conjugation_notes.full_conjugation))
